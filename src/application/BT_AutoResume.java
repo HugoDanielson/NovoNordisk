7f1c7b4241c14ffa94ae01fc5c953eb9f0e772ec
danielbilik <danielbilik@DANIEBILIKFFA5>
2020-01-13 23:14:09 +0900

    --- automatic rebase to remote; old local SHA: 26fad5485195fb9ea4c8fd592538d990586db135 ---

--- src/application/BT_AutoResume.java
+++ src/application/BT_AutoResume.java
@@ -5,6 +5,7 @@ package application;
 import javax.inject.Inject;
 import java.util.concurrent.TimeUnit;
 
+import com.kuka.nav.robot.MobileRobot;
 import com.kuka.roboticsAPI.annotations.AutomaticResumeManager;
 import com.kuka.roboticsAPI.annotations.IAutomaticResumeFunction;
 import com.kuka.roboticsAPI.applicationModel.tasks.CycleBehavior;
@@ -13,8 +14,9 @@ import com.kuka.task.properties.TaskFunctionProvider;
 
 public class BT_AutoResume extends RoboticsAPICyclicBackgroundTask {
 	@Inject
-	private AutomaticResumeManager _automaticResumeManager;
-
+	private AutomaticResumeManager resumeManager;
+	@Inject
+	private MobileRobot kmr;
 	@Override
 	public void initialize() {
 		initializeCyclic(0, 500, TimeUnit.MILLISECONDS,
@@ -23,10 +25,17 @@ public class BT_AutoResume extends RoboticsAPICyclicBackgroundTask {
 
 	@Override
 	public void runCyclic() {
+		
+		if(!kmr.getSafetyState().toString().contentEquals("PROTECTIVE_STOP")){
+			resumeManager.enableApplicationResuming("Test1.class");
+		
+		}
+		
+
 	}
 	
 	@TaskFunctionProvider
 	public IAutomaticResumeFunction getAutomaticResumeFunction() {
-		return _automaticResumeManager;
+		return resumeManager;
 	}
 }
