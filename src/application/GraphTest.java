a8c70e13e02917edf5bdb0dc50e3af2f2cd9e879
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-01 13:13:06 +0800

    --- automatic rebase to remote; old local SHA: d462a1c68b68c33a034acd8ba9e226fb592c2023 ---

---
+++ src/application/GraphTest.java
@@ -0,0 +1,55 @@
+package application;
+
+
+import javax.inject.Inject;
+
+import com.kuka.nav.data.LocationData;
+import com.kuka.nav.fleet.FleetManager;
+import com.kuka.nav.fleet.graph.GraphData;
+import com.kuka.nav.fleet.graph.TopologyGraph;
+import com.kuka.nav.robot.MobileRobotManager;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.deviceModel.kmp.KmpOmniMove;
+import com.kuka.task.ITaskLogger;
+import com.kuka.task.ITaskManager;
+
+
+public class GraphTest extends RoboticsAPIApplication {
+	@Inject
+	private LBR lBR_iiwa_14_R820_1;
+
+	@Inject
+	private KmpOmniMove kMR_omniMove_200_CR_1;
+	@Inject
+	private ITaskLogger logger;
+	@Inject
+	private MobileRobotManager robotManager;
+
+	@Inject
+	private GraphData graphData;
+	@Inject
+	private FleetManager fleetManager;
+	@Inject
+	private LocationData locData;
+	@Inject
+	private ITaskManager taskmanager;
+
+	private TopologyGraph topologyGraph;
+	
+	@Override
+	public void initialize() {
+		// initialize your application here
+	}
+
+	@Override
+	public void run() {
+		// your application execution starts here
+		//lBR_iiwa_14_R820_1.move(ptpHome());
+		topologyGraph = graphData.get(3);
+		System.out.println("Graph ="+topologyGraph);
+		
+		
+	}
+}
