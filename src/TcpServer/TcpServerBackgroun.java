3af2bb1a7abfdade951073d31c8c6232be945e1f
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-01 16:59:00 +0800

    --- automatic rebase to remote; old local SHA: dac02bbb9344fd8f00b6e2ca50d79eccec017f74 ---

--- src/TcpServer/TcpServerBackgroun.java
+++ src/TcpServer/TcpServerBackgroun.java
@@ -2,6 +2,7 @@ package TcpServer;
 
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -11,6 +12,7 @@ import com.kuka.nav.robot.MobileRobot;
 import com.kuka.roboticsAPI.applicationModel.tasks.CycleBehavior;
 import com.kuka.roboticsAPI.applicationModel.tasks.RoboticsAPICyclicBackgroundTask;
 import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.persistenceModel.processDataModel.IProcessData;
 import com.kuka.sunrise.common.task.categories.BackgroundTaskCategory;
 import com.kuka.task.properties.TaskFunctionProvider;
 
@@ -19,6 +21,9 @@ public class TcpServerBackgroun extends RoboticsAPICyclicBackgroundTask implemen
 	
 	@Inject
 	private MobileRobot kmr;
+	@Inject
+	@Named("Error")
+	IProcessData Error;
 	public static TcpServerX tcpServer;
 	private Integer port = 30001;
 	public void initialize() {
@@ -30,9 +35,11 @@ public class TcpServerBackgroun extends RoboticsAPICyclicBackgroundTask implemen
 	public void runCyclic() {
 		if(tcpServer !=null && !tcpServer.isRunning()){
 			tcpServer.connect();
+			Error.setValue("e1");
 		}else {
 			tcpServer = new TcpServerX(port,kmr);
 			tcpServer.connect();
+			Error.setValue("e2");
 		}
 		
 	}
