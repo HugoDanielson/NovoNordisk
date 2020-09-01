2f79571455ac0f3d73122661261a01f8960713dd
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-01 17:13:36 +0800

    --- automatic rebase to remote; old local SHA: e9ad5de90d4aee5b4570e8cd13c9226988646bcf ---

--- src/TcpServer/TcpServerBackgroun.java
+++ src/TcpServer/TcpServerBackgroun.java
@@ -1,12 +1,12 @@
 package TcpServer;
 
-
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
 
 import com.kuka.nav.robot.MobileRobot;
 import com.kuka.roboticsAPI.applicationModel.tasks.CycleBehavior;
@@ -17,9 +17,8 @@ import com.kuka.roboticsAPI.persistenceModel.processDataModel.IProcessData;
 import com.kuka.sunrise.common.task.categories.BackgroundTaskCategory;
 import com.kuka.task.properties.TaskFunctionProvider;
 
-
 public class TcpServerBackgroun extends RoboticsAPIBackgroundTask implements ItcpApi {
-	
+
 	@Inject
 	private MobileRobot kmr;
 	@Inject
@@ -27,40 +26,40 @@ public class TcpServerBackgroun extends RoboticsAPIBackgroundTask implements Itc
 	IProcessData Error;
 	public static TcpServerX tcpServer;
 	private Integer port = 30003;
-	
+	public ExecutorService exServer = Executors.newCachedThreadPool();
 
 	@Override
 	public void run() {
-		tcpServer = new TcpServerX(getApplicationData(),port,kmr);
-		while(true){
-		if(tcpServer !=null && !tcpServer.isRunning()){
-			tcpServer.connect();
-			Error.setValue("e1");
-		}else if(tcpServer ==null) {
-			tcpServer = new TcpServerX(getApplicationData(),port,kmr);
+		tcpServer = new TcpServerX(getApplicationData(), port, kmr);
+		while (true) {
+			if (tcpServer != null && !tcpServer.isRunning()) {
+				exServer.execute(tcpServer);
+
+			} else if (tcpServer == null) {
+				tcpServer = new TcpServerX(getApplicationData(), port, kmr);
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					// e.printStackTrace();
+				}
+				tcpServer.connect();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					// e.printStackTrace();
+				}
+				Error.setValue("e2");
+			}
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
-				//e.printStackTrace();
+				// e.printStackTrace();
 			}
-			tcpServer.connect();
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-			}
-			Error.setValue("e2");
 		}
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		}
-		}
-		
+
 	}
 
 	@Override
@@ -68,7 +67,7 @@ public class TcpServerBackgroun extends RoboticsAPIBackgroundTask implements Itc
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@TaskFunctionProvider
 	public ItcpApi getAppInfoFunction() {
 		return this;
@@ -77,7 +76,8 @@ public class TcpServerBackgroun extends RoboticsAPIBackgroundTask implements Itc
 	public void dispose() {
 
 		tcpServer.stop();
+		exServer.shutdownNow();
 		super.dispose();
 	}
-	
+
 }
