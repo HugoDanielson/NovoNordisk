d6d24482b082d7fe6f95a006a7a524de646f9626
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-01 13:57:34 +0800

    --- automatic rebase to remote; old local SHA: 8f3945d45c398c69a8c71e1e4bb93afc9a901aef ---

--- src/KmrMoves/FromChargerToSt1.java
+++ src/KmrMoves/FromChargerToSt1.java
@@ -1,11 +1,17 @@
 package KmrMoves;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import GlobalParameters.GlobalParam;
 
 import com.kuka.nav.Location;
+import com.kuka.nav.XYTheta;
 import com.kuka.nav.data.LocationData;
+import com.kuka.nav.line.VirtualLineMotion;
+import com.kuka.nav.line.VirtualLineMotionContainer;
 import com.kuka.nav.robot.MobileRobot;
 
 public class FromChargerToSt1 {
@@ -19,8 +25,24 @@ public class FromChargerToSt1 {
 public void move(){
 	Location st5 = location.get(GlobalParam.ST5);
 	
+	List<Location> moves = new ArrayList();
+	
+	moves.add(location.get(GlobalParam.LCHECKPOINT5));
+	moves.add(location.get(GlobalParam.LCHECKPOINT4));
+	moves.add(location.get(GlobalParam.ST4));
+	moves.add(location.get(GlobalParam.LCHECKPOINT2));
+	moves.add(location.get(GlobalParam.LCHECKPOINT1));
+	moves.add(location.get(GlobalParam.ST1));
+	
 	System.out.println("Location Charger = "+st5);
 	System.out.println("KMR battery state = "+kmr.getBatteryState());
+	
+	VirtualLineMotionContainer vcm;
+	
+	for (int i = 0; i < moves.size(); i++) {
+		vcm = kmr.execute(new VirtualLineMotion(kmr.getPose(), moves.get(i).getPose()).setVelocity(new XYTheta(0.1, 0.1, 0.1)));
+		vcm.awaitFinished();
+	}
 	
 	
 	
