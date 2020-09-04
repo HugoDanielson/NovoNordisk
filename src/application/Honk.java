cd97240c8fb60646cc6e5e60d2a256b48a83d94c
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-04 12:03:45 +0800

    --- automatic rebase to remote; old local SHA: 44b10f96e13c4a7734efce0342e59240f973537b ---

---
+++ src/application/Honk.java
@@ -0,0 +1,48 @@
+package application;
+
+
+import javax.inject.Inject;
+
+import com.kuka.nav.honk.HonkAction;
+import com.kuka.nav.honk.HonkCommand;
+import com.kuka.nav.robot.MobileRobot;
+import com.kuka.resource.locking.LockException;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.deviceModel.kmp.KmpOmniMove;
+
+
+public class Honk extends RoboticsAPIApplication {
+	@Inject
+	private LBR lBR_iiwa_14_R820_1;
+	@Inject
+	private MobileRobot kmp;
+	@Inject
+	private KmpOmniMove kMR_omniMove_200_CR_1;
+
+	@Override
+	public void initialize() {
+		// initialize your application here
+	}
+
+	@Override
+	public void run() {
+		HonkAction honkAction = new HonkAction(HonkCommand.ON). setContinuous(true).setHonkTimeDuration(2000);
+		
+
+			try {
+				kmp.lock();
+			} catch (LockException e) {
+				// TODO Auto-generated catch block
+			//	e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+			//	e.printStackTrace();
+			}
+			
+			kmp.execute(honkAction);
+			kmp.unlock();
+		
+	}
+}
--- src/application/TestApp.java
+++ src/application/TestApp.java
@@ -10,24 +10,7 @@ import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.deviceModel.kmp.KmpOmniMove;
 import com.kuka.roboticsAPI.geometricModel.Tool;
 
-/**
- * Implementation of a robot application.
- * <p>
- * The application provides a {@link RoboticsAPITask#initialize()} and a 
- * {@link RoboticsAPITask#run()} method, which will be called successively in 
- * the application lifecycle. The application will terminate automatically after 
- * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
- * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
- * exception is thrown during initialization or run. 
- * <p>
- * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
- * {@link RoboticsAPITask#dispose()} method.</b> 
- * 
- * @see UseRoboticsAPIContext
- * @see #initialize()
- * @see #run()
- * @see #dispose()
- */
+
 public class TestApp extends RoboticsAPIApplication {
 	@Inject
 	private LBR lbr;
