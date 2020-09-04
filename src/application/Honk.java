58812440aec4378d0605e633aaf60f31852f4a0f
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-04 12:04:44 +0800

    --- automatic rebase to remote; old local SHA: e2d7f371e84c615ffc1e6b75e9df21b89b802843 ---

--- src/application/Honk.java
+++ src/application/Honk.java
@@ -42,6 +42,12 @@ public class Honk extends RoboticsAPIApplication {
 			}
 			
 			kmp.execute(honkAction);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			kmp.unlock();
 		
 	}
