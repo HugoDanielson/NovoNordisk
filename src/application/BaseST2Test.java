f262310562c0f3e1cb6b5d548398ebac4fed2af4
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-07 15:32:22 +0800

    --- automatic rebase to remote; old local SHA: aaa2233985cdc6586ab0869d246a9e578ef3a927 ---

--- src/application/BaseST2Test.java
+++ src/application/BaseST2Test.java
@@ -33,12 +33,14 @@ public class BaseST2Test extends RoboticsAPIApplication {
 		Gripper2.attachTo(lbr.getFlange());
 
 		tcp = Gripper2.getFrame("TCP2/AngleOffset/ShiftTCP1");
-
+		System.out.println("Robot in moving to P1");
 		tcp.move(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P1")).setJointVelocityRel(0.2));
+		System.out.println("Robot in moving to P2");
 		tcp.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P2")).setJointVelocityRel(0.2));
 		System.out.println("Robot in PickUp position");
 		waitSec(2000);
 		System.out.println("Robot in start motion");
+		System.out.println("Robot in moving to P1");
 		tcp.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P1")).setJointVelocityRel(0.2));
 
 	}
