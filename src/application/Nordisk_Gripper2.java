b7c8bd4c693228611371105a8d3a16632f726d31
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-08 14:28:42 +0800

    --- automatic rebase to remote; old local SHA: cf17ad73042ab075ce25110989659f368f74ad6e ---

--- src/application/Nordisk_Gripper2.java
+++ src/application/Nordisk_Gripper2.java
@@ -134,15 +134,15 @@ public class Nordisk_Gripper2 extends RoboticsAPIApplication {
 		
 		tcp3 = Gripper2.getFrame("TCP2/AngleOffset/ShiftTCP3");
 		
-		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P12")).setJointVelocityRel(0.2));
+		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P12")).setJointVelocityRel(0.1));
 
-		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P13")).setJointVelocityRel(0.2));
+		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P13")).setJointVelocityRel(0.1));
 		waitSec(8000);
-		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P14")).setJointVelocityRel(0.2));
+		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P14")).setJointVelocityRel(0.1));
 		waitSec(8000);
-		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P15")).setJointVelocityRel(0.2));
+		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P15")).setJointVelocityRel(0.1));
 		waitSec(8000);
-		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P16")).setJointVelocityRel(0.2));
+		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P16")).setJointVelocityRel(0.1));
 		waitSec(8000);
 		
 		tcp3.move(lin(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/ZCalibration/P16")).setJointVelocityRel(0.2));
