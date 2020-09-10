7b32b76b7e35807e03ab00c3f338df7944de7b5f
danielbilik <danielbilik@DANIELBILIK4939>
2020-09-10 09:46:43 +0800

    --- automatic rebase to remote; old local SHA: 5c8f38f2d77b81be60264c45b6f2a71db3363891 ---

--- src/application/HugoTest.java
+++ src/application/HugoTest.java
@@ -14,24 +14,7 @@ import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.deviceModel.kmp.KmpOmniMove;
 
-/**
- * Implementation of a robot application.
- * <p>
- * The application provides a {@link RoboticsAPITask#initialize()} and a 
- * {@link RoboticsAPITask#run()} method, which will be called successively in 
- * the application lifecycle. The application will terminate automatically after 
- * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
- * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
- * exception is thrown during initialization or run. 
- * <p>
- * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
- * {@link RoboticsAPITask#dispose()} method.</b> 
- * 
- * @see UseRoboticsAPIContext
- * @see #initialize()
- * @see #run()
- * @see #dispose()
- */
+
 public class HugoTest extends RoboticsAPIApplication {
 	@Inject
 	private LBR lBR_iiwa_14_R820_1;
@@ -55,6 +38,7 @@ private MapZoneData mapData;
 		
 		for (MapZone mapZone : data) {
 			System.out.println("MapData = "+mapZone.getName());
+			System.out.println("Robot is inside =" +mapZone.isInside(kmp));
 		}
 		
 	}
