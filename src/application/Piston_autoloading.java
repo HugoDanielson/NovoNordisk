71efac50063016ebcd557429d16af8a045c28782
ZCQY <ZCQY@DESKTOP-HOO1KFQ>
2020-09-11 16:22:55 +0800

    --- automatic rebase to remote; old local SHA: 886c630fd7fe756918f346d5749e2337ce111ceb ---

--- src/application/Piston_autoloading.java
+++ src/application/Piston_autoloading.java
@@ -133,7 +133,7 @@ public class Piston_autoloading extends RoboticsAPIApplication {
 		
 		//***move AGV to Charger from anywhere***//
 		VirtualLineMotionContainer vcm;
-		vcm = kmr.execute(new VirtualLineMotion(kmr.getPose(), locData.get(5).getPose()).setVelocity( new XYTheta(0.1, 0.1, 0.1)));
+		vcm = kmr.execute(new VirtualLineMotion(kmr.getPose(), locData.get(5).getPose()).setVelocity( new XYTheta(0.5, 0.5, 0.5)));
 		vcm.awaitFinished();
 		
 		//Finetune
@@ -158,7 +158,7 @@ public class Piston_autoloading extends RoboticsAPIApplication {
 		double y = 0.0; 
 		double theta = Math.toRadians(0);
 		RelativeMotion motion = new RelativeMotion(x, y, theta);
-		kmr.execute(motion.setVelocity(new XYTheta(0.05, 0.05, 0.05)));
+		kmr.execute(motion.setVelocity(new XYTheta(0.5, 0.5, 0.5)));
 		moveTo.run(eMoveFrom.St5, eMoveTo.St2, null);
 		moveFineLocation(locData.get(2), 0.1, kmr);
 		
@@ -173,7 +173,7 @@ public class Piston_autoloading extends RoboticsAPIApplication {
 		tcp2.moveAsync(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/P6")).setJointVelocityRel(0.3));
 
 		//move AGV from T-BOX(st2) to piston loading place(st4)
-		kmr.execute(motion.setVelocity(new XYTheta(0.2, 0.2, 0.2)));
+		kmr.execute(motion.setVelocity(new XYTheta(0.4, 0.4, 0.4)));
 		moveTo.run(eMoveFrom.St2, eMoveTo.St4, null);
 		moveFineLocation(locData.get(4), 0.1, kmr);
 		
@@ -186,18 +186,18 @@ public class Piston_autoloading extends RoboticsAPIApplication {
 		tcp2.move(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/P7")).setBlendingCart(20).setJointVelocityRel(0.1));
 
 		//back to T-BOX(st2)
-		kmr.execute(motion.setVelocity(new XYTheta(0.2, 0.2, 0.2)));
+		kmr.execute(motion.setVelocity(new XYTheta(0.5, 0.5, 0.5)));
 		moveTo.run(eMoveFrom.St4, eMoveTo.St2, null);
 		moveFineLocation(locData.get(2), 0.1, kmr);
 		
 		//put down the support box
-		//tcp2.move(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/P10")).setBlendingCart(20).setJointVelocityRel(0.3));
-		//tcp2.move(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/P11")).setBlendingCart(20).setJointVelocityRel(0.3));
+		tcp2.move(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/P10")).setBlendingCart(20).setJointVelocityRel(0.3));
+		tcp2.move(ptp(getApplicationData().getFrame("/Station2/BaseShift/CameraOffset/P11")).setBlendingCart(20).setJointVelocityRel(0.3));
 
 		//back to Charger(st5)
-		//kmr.execute(motion.setVelocity(new XYTheta(0.2, 0.2, 0.2)));
-		//moveTo.run(eMoveFrom.St2, eMoveTo.St5, null);
-		//moveFineLocation(locData.get(5), 0.1, kmr);
+		kmr.execute(motion.setVelocity(new XYTheta(0.5, 0.5, 0.5)));
+		moveTo.run(eMoveFrom.St2, eMoveTo.St5, null);
+		moveFineLocation(locData.get(5), 0.1, kmr);
 		
 		kmr.unlock();
 		
